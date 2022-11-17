1b464ef5cc6adcf7c7997e11be7a0f51f8c6a2af
张瑞康 <2919038302@qq.com>
2022-11-17 17:29:49 +0800

    '提交首页和我的五个页面jscss'

--- js/imdex2.js
+++ js/imdex2.js
@@ -2,20 +2,6 @@
 var freeDownload = document.getElementsByClassName("freeDownload")[0];
 // 获取X号
 var close_ = document.getElementsByClassName("close")[0];
-touch.on(close_, "tap", function () {
+close_.onclick = function () {
 	freeDownload.parentNode.removeChild(freeDownload);
-});
-
-// 获取我的按钮
-let myBtn = document.getElementsByClassName("myBtn")[0];
-console.log(myBtn);
-// 判断用户是否登录成功
-if (window.localStorage.getItem("isLogin")) {
-	touch.on(myBtn, "tap", function () {
-		window.location.href = "./my/myLogin.html";
-	});
-} else {
-	touch.on(myBtn, "tap", function () {
-		window.location.href = "./my/my.html";
-	});
 }
